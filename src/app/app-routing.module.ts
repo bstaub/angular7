05d94d48e76fd85bf85ca182ccaa1c92b40c97f6
Bruno Staub <bruno.staub@gmail.com>
2018-10-30 20:07:04 +0100

    base universal 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ProductsComponent} from './products/products.component';
+import {ProductComponent} from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductsComponent},
+  {path: 'product/:id', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
